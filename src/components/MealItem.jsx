4a5e5e70cad0b51087c64337d438fb48f34118cf
Gilang <gilangardhi87@gmail.com>
2025-04-24 16:13:49 +0700

    fitur cart

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,18 +1,37 @@
+import { useContext } from "react";
+
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
+
 export default function MealItem({ image, name, price, description }) {
-    return (
-      <li className="meal-item">
-        <article>
-          <img src={`http://localhost:3000/${ image }`} alt={name} />
-          <div>
-            <h3>{name}</h3>
-            <p className="meal-item-price">{price}</p>
-            <p className="meal-item-description">{description}</p>
-          </div>
-          <p className="meal-item-actions">
-            <button>Add to Cart</button>
-          </p>
-        </article>
-      </li>
-    );
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    const meal = {
+      id: name, // gunakan ID unik jika tersedia!
+      name,
+      image,
+      price,
+      description,
+    };
+
+    cartCtx.addItem(meal);
   }
-  
+
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${image}`} alt={name} />
+        <div>
+          <h3>{name}</h3>
+          <p className="meal-item-price">{currencyFormatter.format(price)}</p>
+          <p className="meal-item-description">{description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
+        </p>
+      </article>
+    </li>
+  );
+}
